d1f5dd09621c4ac907332c8b751389bce42cffce
WMight <wwwyf123456789@163.com>
2020-07-21 20:14:41 +0800

    用vue-cli3实现左边导航右边切换相应内容

---
+++ src/components/Goodlist.vue
@@ -0,0 +1,71 @@
+<template>
+    <div name="show">
+        <ul class="goodlist">
+            <li v-for="(goods,index) in list" :key="index">
+                <img v-bind:src="goods.img" alt="" width="220px" height="220px">
+                <p>{{goods.goodName}}</p>
+            </li>
+        </ul>
+    </div>    
+</template>
+
+<script type="text/javascript">
+export default {
+    name:"show",
+    data(){
+        var _this = this;
+        var url = "";
+        if(_this.goodId==1){
+            url="json/bjb.json";
+        }else if(_this.goodId==2){
+            url="json/sj.json";
+        }else{
+            url="json/bjb.json";
+        }
+        this.$http.get(url).then(function(res){
+            _this.list = res.data;
+        })
+        return{
+            list:[]
+        }
+    },
+    props:{
+        goodId:Number
+    },
+    watch:{
+        goodId(){
+            var _this = this;
+            var url = "";
+            if(_this.goodId==1){
+                url="json/bjb.json";
+            }else if(_this.goodId==2){
+                url="json/sj.json";
+            }else{
+                url="json/bjb.json";
+            }
+            this.$http.get(url).then(function(res){
+                _this.list = res.data;
+            })
+            return{
+                list:[]
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    .goodlist li{
+        width: 200px;
+        height: 200px;
+        list-style: none;
+        float: left;
+        font-size: 9px;
+        color: red;
+        margin-bottom: 30px;
+    }
+    .goodlist img{
+        width: 180px;
+        height: 180px;
+    }
+</style>
---
+++ src/components/Right.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <div v-if="kk==1">
+            <Goodlist :goodId="1"></Goodlist>
+        </div>
+        <div v-else-if="kk==2">
+            <Goodlist :goodId="2"></Goodlist>
+        </div>
+        <div v-else-if="kk==3">
+            3333333333
+        </div>
+        <div v-else-if="kk==4">
+            4444444444
+        </div>
+        <div v-else-if="kk==5">
+            5555555
+        </div>
+        <div v-else-if="kk==6">
+            6666666
+        </div>
+        <div v-else-if="kk==7">
+            777777777777
+        </div>
+        <div v-else-if="kk==8">
+            88888888
+        </div>
+        <div v-else-if="kk==9">
+            999999999999
+        </div>
+        <div v-else-if="kk==10">
+            10
+        </div>
+        <div v-else-if="kk==11">
+            11
+        </div>
+        <div v-else>
+            <Goodlist :goodId="0"></Goodlist>
+        </div>
+    </div>
+</template>
+
+<script>
+import Msg from './Msg.js'
+import Goodlist from './Goodlist.vue'
+export default {
+    data(){
+        return {
+            kk:0
+        }
+    },
+    mounted:function(){
+        var _this = this
+        Msg.$on('val',function(m){
+            _this.kk = m;
+        })
+    },
+    components:{
+        Goodlist
+    }
+}
+</script>
+
+<style>
+
+</style>
